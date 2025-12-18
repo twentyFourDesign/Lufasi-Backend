a6db5f1909a55b7b3f6b2605cae737e21f6f63b4
Muhammad-Usama-Mujeeb-1 <usamamujeeb1@gmail.com>
2025-12-18 13:35:58 +0500

    feat: Update database configuration to use Supabase connection URL

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.SUPABASE_DB_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
